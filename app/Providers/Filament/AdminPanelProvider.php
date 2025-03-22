1c010598c9010db07eb30e3edb0edc0b05b1e048
chaidarsaad <chaidarsaad55@gmail.com>
2025-03-23 00:45:51 +0700

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -9,6 +9,7 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\MenuItem;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -60,6 +61,11 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 // Authenticate::class,
                 RedirectIfNotFilamentAdmin::class,
+            ])
+            ->userMenuItems([
+                MenuItem::make()
+                    ->label('Home')
+                    ->url('/', shouldOpenInNewTab: true)
             ])
             ->spa()
             ->unsavedChangesAlerts()
