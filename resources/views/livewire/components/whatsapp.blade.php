561dacb894a5caf22dea9b4e2b0f6b14121a94d1
chaidarsaad <chaidarsaad55@gmail.com>
2025-03-23 08:39:04 +0700

    update: sticky whatsapp

--- resources/views/livewire/components/whatsapp.blade.php
+++ resources/views/livewire/components/whatsapp.blade.php
@@ -1,5 +1,5 @@
 <div class="whatsapp-popup">
-    <a href="#">
+    <a href="https://wa.me/+62{{ $about->phone_number }}" target="_blank">
         <i class="fa-brands fa-square-whatsapp whatsapp-popup-fa"></i>
     </a>
 </div>
