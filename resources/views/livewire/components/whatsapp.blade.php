de9c50aee33b7bd73092468d33e911d844617336
chaidarsaad <chaidarsaad55@gmail.com>
2025-03-28 23:12:29 +0700

    update

--- resources/views/livewire/components/whatsapp.blade.php
+++ resources/views/livewire/components/whatsapp.blade.php
@@ -22,7 +22,9 @@
 <div>
     @if (!empty($about->phone_number))
         <div class="whatsapp-popup">
-            <p>HUBUNGI KAMI</p>
+            <a href="https://wa.me/+62{{ $about->phone_number }}">
+                <p>HUBUNGI KAMI</p>
+            </a>
             <a href="https://wa.me/+62{{ $about->phone_number }}" target="_blank">
                 <i class="fa-brands fa-square-whatsapp whatsapp-popup-fa" style="color: #0105da"></i>
             </a>
