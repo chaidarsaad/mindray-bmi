b427fc65a8efddc71ee87f909226b719202f3e28
chaidarsaad <chaidarsaad55@gmail.com>
2025-03-29 00:41:23 +0700

    update

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,7 +1,7 @@
    <!-- font -->
    <link rel="stylesheet" href="{{ asset('assets/fonts/fonts.css') }}" />
    <link rel="stylesheet" href="{{ asset('assets/fonts/font-icons.css') }}" />
-   <link rel="stylesheet" href="{{ asset('assets/css/bootstrapext1.min.css') }}" />
+   <link rel="stylesheet" href="{{ asset('assets/css/bootstrapext2.min.css') }}" />
    <link rel="stylesheet" href="{{ asset('assets/css/swiper-bundle.min.css') }}" />
    <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}" />
    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/main5.css') }}" />
