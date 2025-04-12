2fa39a86b91622e5e8508aab6ca7ce72c5ee4072
chaidarsaad <chaidarsaad55@gmail.com>
2025-04-12 15:00:14 +0700

    update

--- resources/views/livewire/training-payment-page.blade.php
+++ resources/views/livewire/training-payment-page.blade.php
@@ -52,6 +52,14 @@
             max-width: 100%;
             object-fit: contain;
         }
+
+        .file-name {
+            max-width: 100%;
+            display: inline-block;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
     </style>
 @endpush
 
