d48b41ac9f24c0392ee1f0f92e28543fc823a06d
chaidarsaad <chaidarsaad55@gmail.com>
2025-04-14 22:28:21 +0700

    update

--- app/Livewire/DetailProductPage.php
+++ app/Livewire/DetailProductPage.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\About;
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class DetailProductPage extends Component
@@ -12,6 +13,7 @@ class DetailProductPage extends Component
     public $otherProducts;
     public $productDescriptions;
     public $about;
+    public $whatsappMessage;
 
     public function mount($slug)
     {
@@ -20,6 +22,18 @@ class DetailProductPage extends Component
         $this->productDescriptions = $this->product->descriptions;
 
         $this->about = About::first();
+        if (Auth::check()) {
+            $user = Auth::user();
+            $this->whatsappMessage =  'Halo admin 👋,%0A%0A' .
+                'Saya tertarik untuk memesan alat USG berikut:%0A%0A' .
+                '🛒 Nama Produk: ' . $this->product->name . '%0A%0A' .
+                'Berikut data saya:%0A' .
+                '👤 Nama: ' . $user->name . '%0A' .
+                '📧 Email: ' . $user->email . '%0A%0A' .
+                'Mohon informasi lebih lanjut, terima kasih 🙏';
+        } else {
+            $this->whatsappMessage = null;
+        }
     }
     public function render()
     {
