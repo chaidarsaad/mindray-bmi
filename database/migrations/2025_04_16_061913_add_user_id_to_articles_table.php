395e4fe805a65299babfab5af3f7b279a9ce5377
chaidarsaad <chaidarsaad55@gmail.com>
2025-04-16 06:28:33 +0700

    update

--- database/migrations/2025_04_16_061913_add_user_id_to_articles_table.php
+++ database/migrations/2025_04_16_061913_add_user_id_to_articles_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('articles', function (Blueprint $table) {
-            $table->foreignId('user_id')->after('id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->after('id')->constrained()->onDelete('cascade');
         });
     }
 
