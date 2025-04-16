a810512545ec15de1c719804d9d7ba77682aa8d4
chaidarsaad <chaidarsaad55@gmail.com>
2025-04-16 13:51:31 +0700

    update

--- app/Livewire/Components/Training.php
+++ app/Livewire/Components/Training.php
@@ -13,14 +13,16 @@ class Training extends Component
     public function mount()
     {
         // $this->trainings = ModelsTraining::where('is_show', 1)
-        //     ->orderBy('tanggal', 'desc')
+        //     ->join('training_prices', 'trainings.id', '=', 'training_prices.training_id')
+        //     ->orderByDesc('training_prices.start_date')
+        //     ->select('trainings.*')
+        //     ->distinct()
+        //     ->take(4)
         //     ->get();
-
         $this->trainings = ModelsTraining::where('is_show', 1)
-            ->join('training_prices', 'trainings.id', '=', 'training_prices.training_id')
-            ->orderByDesc('training_prices.start_date')
-            ->select('trainings.*')
-            ->distinct()
+            ->join(DB::raw('(SELECT training_id, MAX(start_date) as latest_date FROM training_prices GROUP BY training_id) as tp'), 'trainings.id', '=', 'tp.training_id')
+            ->orderByDesc('tp.latest_date')
+            ->select('trainings.*', 'tp.latest_date')
             ->take(4)
             ->get();
     }
--- app/Livewire/TrainingPage.php
+++ app/Livewire/TrainingPage.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Training;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class TrainingPage extends Component
@@ -10,11 +11,23 @@ class TrainingPage extends Component
     public $trainings;
     public function mount()
     {
+        // $this->trainings = Training::where('is_show', 1)
+        //     ->join('training_prices', 'trainings.id', '=', 'training_prices.training_id')
+        //     ->orderByDesc('training_prices.start_date')
+        //     ->select('trainings.*') // Pilih kolom dari tabel trainings
+        //     ->distinct() // Menghindari duplikasi
+        //     ->get();
+
         $this->trainings = Training::where('is_show', 1)
-            ->join('training_prices', 'trainings.id', '=', 'training_prices.training_id')
-            ->orderByDesc('training_prices.start_date')
-            ->select('trainings.*') // Pilih kolom dari tabel trainings
-            ->distinct() // Menghindari duplikasi
+            ->join(
+                DB::raw('(SELECT training_id, MAX(start_date) as latest_start_date FROM training_prices GROUP BY training_id) as tp'),
+                'trainings.id',
+                '=',
+                'tp.training_id'
+            )
+            ->orderByDesc('tp.latest_start_date')
+            ->select('trainings.*', 'tp.latest_start_date')
+            ->limit(4)
             ->get();
     }
     public function render()
