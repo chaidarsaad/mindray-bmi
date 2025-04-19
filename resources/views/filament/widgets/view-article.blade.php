6f2be3813a03136b57e15fac163821ee780ad83d
chaidarsaad <chaidarsaad55@gmail.com>
2025-04-19 11:48:52 +0700

    update

--- resources/views/filament/widgets/view-article.blade.php
+++ resources/views/filament/widgets/view-article.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div class="max-h-[70vh] overflow-y-auto px-4 py-2">
     <div class="text-sm mb-2">
         Ditulis oleh: {{ optional($record->user)->name ?? 'Admin' }} |
         {{ $record->created_at->translatedFormat('l, d F Y') }}
