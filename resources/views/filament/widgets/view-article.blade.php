dba0ab026e315add17614e9501e6545a963b4b1c
chaidarsaad <chaidarsaad55@gmail.com>
2025-04-22 09:54:16 +0700

    update

--- resources/views/filament/widgets/view-article.blade.php
+++ resources/views/filament/widgets/view-article.blade.php
@@ -1,12 +1,12 @@
 <div>
-    <div class="text-sm mb-2">
+    <div class="mb-2">
         Ditulis oleh: {{ optional($record->user)->name ?? 'Admin' }} |
         {{ $record->created_at->translatedFormat('l, d F Y') }}
     </div>
 
     <img src="{{ asset('storage/' . $record->image) }}" alt="{{ $record->judul }}" class="w-full h-auto rounded mb-4">
 
-    <div class=" max-w-none">
+    <div class="max-w-none">
         {!! $record->content !!}
     </div>
 </div>
