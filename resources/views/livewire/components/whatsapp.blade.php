5d8f41ec682855b9f405f4c67c20bd3339b4081e
chaidarsaad <chaidarsaad55@gmail.com>
2025-04-25 11:15:09 +0700

    update

--- resources/views/livewire/components/whatsapp.blade.php
+++ resources/views/livewire/components/whatsapp.blade.php
@@ -30,20 +30,11 @@
 
 <div>
     @if (!empty($about->phone_number))
-        <div class="whatsapp-chat">
+        <a class="whatsapp-chat" aria-label="WhatsApp chat" href="https://wa.me/+62{{ $about->phone_number }}"
+            target="_blank" rel="noopener noreferrer">
             <span>HUBUNGI KAMI</span>
-            <a aria-label="WhatsApp chat" href="https://wa.me/+62{{ $about->phone_number }}" target="_blank"
-                rel="noopener noreferrer">
-                <i class="fab fa-whatsapp"></i>
-            </a>
-        </div>
-        {{-- <div class="whatsapp-popup">
-            <a href="https://wa.me/+62{{ $about->phone_number }}">
-                <p>HUBUNGI KAMI</p>
-            </a>
-            <a href="https://wa.me/+62{{ $about->phone_number }}" target="_blank">
-                <i class="fa-brands fa-square-whatsapp whatsapp-popup-fa" style="color: #0105da"></i>
-            </a>
-        </div> --}}
+            <i class="fab fa-whatsapp" style="font-size: 1.5rem;"></i>
+        </a>
     @endif
+
 </div>
