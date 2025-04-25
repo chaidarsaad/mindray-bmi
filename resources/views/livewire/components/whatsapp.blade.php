2bc48d99d4082e630a4f0a3989fd41548a3a5f02
chaidarsaad <chaidarsaad55@gmail.com>
2025-04-25 11:07:13 +0700

    update

--- resources/views/livewire/components/whatsapp.blade.php
+++ resources/views/livewire/components/whatsapp.blade.php
@@ -1,24 +1,28 @@
 @push('styles')
     <style>
-        .whatsapp-popup {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            padding: 1rem;
-            border-radius: 8px;
-            box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
-        }
-
-        .whatsapp-popup p {
-            color: #0105da;
-            text-align: center;
-            font-size: 16px;
+        .whatsapp-chat {
+            position: fixed;
+            bottom: 80px;
+            right: 15px;
+            background: #0105da;
+            border-radius: 9999px;
+            padding: 0.25rem 0.75rem;
+            box-shadow: 0 0.125rem 0.25rem rgb(0 0 0 / 0.1);
+            font-size: 0.75rem;
             font-weight: bold;
-            margin-bottom: 0.5rem;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+            color: white;
+            display: flex;
+            align-items: center;
+            gap: 0.5rem;
+            user-select: none;
+            z-index: 100;
         }
 
-        .whatsapp-popup-fa {
-            font-size: 2rem;
+        .whatsapp-chat a {
+            color: white;
+            font-size: 1.25rem;
+            text-decoration: none;
         }
     </style>
 @endpush
@@ -26,13 +30,20 @@
 
 <div>
     @if (!empty($about->phone_number))
-        <div class="whatsapp-popup">
+        <div class="whatsapp-chat">
+            <span>HUBUNGI KAMI</span>
+            <a aria-label="WhatsApp chat" href="https://wa.me/+62{{ $about->phone_number }}" target="_blank"
+                rel="noopener noreferrer">
+                <i class="fab fa-whatsapp"></i>
+            </a>
+        </div>
+        {{-- <div class="whatsapp-popup">
             <a href="https://wa.me/+62{{ $about->phone_number }}">
                 <p>HUBUNGI KAMI</p>
             </a>
             <a href="https://wa.me/+62{{ $about->phone_number }}" target="_blank">
                 <i class="fa-brands fa-square-whatsapp whatsapp-popup-fa" style="color: #0105da"></i>
             </a>
-        </div>
+        </div> --}}
     @endif
 </div>
