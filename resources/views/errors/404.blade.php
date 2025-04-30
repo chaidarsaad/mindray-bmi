1391bb2f1a513395c880f2d715105556ff8bee71
chaidarsaad <chaidarsaad55@gmail.com>
2025-04-30 09:09:31 +0700

    update: not found page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -13,6 +13,51 @@
     @livewireStyles()
     @include('includes.style')
     @stack('styles')
+
+    <style>
+        footer ul,
+        footer li {
+            list-style: none !important;
+            padding-left: 0 !important;
+        }
+
+        footer .footer-infor ul {
+            list-style-type: none !important;
+            padding-left: 0 !important;
+        }
+
+        .whatsapp-chat {
+            position: fixed;
+            bottom: 80px;
+            right: 15px;
+            background: #0105da;
+            border-radius: 9999px;
+            padding: 0.25rem 0.75rem;
+            box-shadow: 0 0.125rem 0.25rem rgb(0 0 0 / 0.1);
+            font-size: 0.75rem;
+            font-weight: bold;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+            color: white;
+            display: flex;
+            align-items: center;
+            gap: 0.5rem;
+            user-select: none;
+            z-index: 100;
+        }
+
+        .whatsapp-chat a {
+            color: white;
+            font-size: 1.25rem;
+            text-decoration: none;
+        }
+
+        @media (min-width: 768px) {
+            .whatsapp-chat {
+                bottom: 120px;
+                right: 30px;
+            }
+        }
+    </style>
 </head>
 
 <body class="preload-wrapper">
