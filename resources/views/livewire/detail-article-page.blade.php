63ec19e1873a19a00f6fc0102c894f2aa48f12ba
chaidarsaad <chaidarsaad55@gmail.com>
2025-06-13 17:40:12 +0700

    update

--- resources/views/livewire/detail-article-page.blade.php
+++ resources/views/livewire/detail-article-page.blade.php
@@ -140,7 +140,7 @@
                                 </div>
 
                                 <div class="meta">
-                                    {{ $article->created_at->locale('id')->isoFormat('dddd, D MMMM YYYY') }}
+                                    {{ $article->published_at->locale('id')->isoFormat('dddd, D MMMM YYYY') }}
                                 </div>
 
                                 <div class="share-buttons"
